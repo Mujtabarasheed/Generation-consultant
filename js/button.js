01b068cfd3c50739d670e1891dbe0aef3f8af614
mujtaba_Rasheed <mujtaba2311a@aptechgdn.net>
2024-08-06 02:35:23 -0700

    generation consultant is ready

---
+++ js/button.js
@@ -0,0 +1,18 @@
+function update_position(e) {
+	let _t = e.target;
+	
+	if(_t.tagName.match(/^immigation$/i)) {
+		let r = _t.getBoundingClientRect();
+		['x', 'y'].forEach(c => 
+			_t.style.setProperty(`--${c}`, 
+													 `${e[`client${c.toUpperCase()}`] - r[c]}px`))
+	}
+}
+
+addEventListener('mouseover', update_position);
+addEventListener('mouseout', update_position)
+
+
+// faq
+
+
